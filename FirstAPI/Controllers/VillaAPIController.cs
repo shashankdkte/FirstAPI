d67b3af4f44d98bcde81deeb5d5e17e6e5d1a4cb
Shashank Dhakate <shashankdkte@gmail.com>
2023-12-11 19:53:21 +0530

    Response Types

--- FirstAPI/Controllers/VillaAPIController.cs
+++ FirstAPI/Controllers/VillaAPIController.cs
@@ -9,12 +9,19 @@ namespace FirstAPI.Controllers
     public class VillaAPIController:ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
             return Ok(VillaStore.villaList);
         }
 
         [HttpGet("{id:int}")]
+        //[ProducesResponseType(200)]
+        //[ProducesResponseType(404)]
+        //[ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> GetVilla(int id)
 
         {
