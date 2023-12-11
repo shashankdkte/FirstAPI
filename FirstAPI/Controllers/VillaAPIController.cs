6a43e34bbda92c6c9cd5d682a19fc2c26b7481fa
Shashank Dhakate <shashankdkte@gmail.com>
2023-12-11 20:25:33 +0530

    Http Delete In Action

--- FirstAPI/Controllers/VillaAPIController.cs
+++ FirstAPI/Controllers/VillaAPIController.cs
@@ -66,5 +66,26 @@ namespace FirstAPI.Controllers
             VillaStore.villaList.Add(villaDTO);
             return CreatedAtRoute("GetVilla", new { id = villaDTO.Id},villaDTO);
         }
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult DeleteVilla(int id) {
+
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa  = VillaStore.villaList.FirstOrDefault(u=>u.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        
+        }
     }
 }
