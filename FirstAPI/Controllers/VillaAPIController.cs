0011041ab9c67053e62a21b7ce52875fae52b563
Shashank Dhakate <shashankdkte@gmail.com>
2023-12-19 19:32:50 +0530

    Entity Framework Core Database Models

--- FirstAPI/Controllers/VillaAPIController.cs
+++ FirstAPI/Controllers/VillaAPIController.cs
@@ -1,5 +1,4 @@
 ﻿using FirstAPI.Data;
-using FirstAPI.Logging;
 using FirstAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,13 +8,9 @@ namespace FirstAPI.Controllers
     [ApiController]
     public class VillaAPIController:ControllerBase
     {
-        //private readonly ILogger<VillaAPIController> _logger;
-        private readonly ILogging _logger;
-        //public VillaAPIController(ILogger<VillaAPIController> logger)
-        //{
-        //    this._logger = logger;
-        //}
-        public VillaAPIController(ILogging logger)
+        private readonly ILogger<VillaAPIController> _logger;
+
+        public VillaAPIController(ILogger<VillaAPIController> logger)
         {
             this._logger = logger;
         }
@@ -23,8 +18,7 @@ namespace FirstAPI.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            //_logger.LogInformation("Getting All Villas");
-            _logger.Log("Getting All Values", "");
+            _logger.LogInformation("Getting All Villas");
             return Ok(VillaStore.villaList);
         }
 
@@ -40,8 +34,7 @@ namespace FirstAPI.Controllers
         {
             if (id == 0)
             {
-                //_logger.LogError("Get Villa with Id Error for " +  id);
-                _logger.Log("Get Villa with Id Error for " +  id, "error");
+                _logger.LogError("Get Villa with Id Error for " +  id);
                 return BadRequest();
             }
 
