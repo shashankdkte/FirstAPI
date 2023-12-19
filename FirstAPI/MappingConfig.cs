1a97c9d155f71f1089211a1704cc06c8206e3d45
Shashank Dhakate <shashankdkte@gmail.com>
2023-12-19 20:54:48 +0530

    Setup AutoMapper and Mapping Config

---
+++ FirstAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FirstAPI.Models;
+using FirstAPI.Models.DTO;
+
+namespace FirstAPI
+{
+    public class MappingConfig:Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa,VillaDTO>();
+            CreateMap<VillaDTO, Villa>();
+
+            CreateMap<Villa, VillaCreateDTO>().ReverseMap();
+            CreateMap<Villa, VillaUpdateDTO>().ReverseMap();
+        }
+    }
+}
--- FirstAPI/Program.cs
+++ FirstAPI/Program.cs
@@ -1,3 +1,4 @@
+using FirstAPI;
 using FirstAPI.Data;
 using FirstAPI.Logging;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
