82091e20dfc92f04d25b1968727dd5b9e3d70628
Shashank Dhakate <shashankdkte@gmail.com>
2023-12-19 19:44:53 +0530

    Connection String and DbSet Entity

---
+++ FirstAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using FirstAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- FirstAPI/Models/Villa.cs
+++ FirstAPI/Models/Villa.cs
@@ -1,7 +1,12 @@
-﻿namespace FirstAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstAPI.Models
 {
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string  Details { get; set; }
